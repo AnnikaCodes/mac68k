0d5954381c9f4c3fd8b6daef6ed38fa13254fbb0
Annika L <annika0uwu@gmail.com>
2022-04-26 20:16:48 -0700

    Improve build script

--- build.sh
+++ build.sh
@@ -10,16 +10,18 @@ function green_echo() {
 
 green_echo "Cleaning build directory..."
 mkdir -p build
-rm build/*
 
 green_echo "Compiling Zig code..."
-zig build-obj -target m68k-freestanding src/enter.zig -fno-LLVM -ofmt=c -femit-bin=build/enter.c
+~/zig/build/zig build-obj -Iinclude/ -target m68k-freestanding src/enter.zig -fno-LLVM -ofmt=c -femit-bin=build/enter.c
 # yes, I hate this too, but Zig expects 128-bit int types that don't exist on m68k
-sed -i'.bak' 's/^.*int128_t.*$/ /g' build/*.c
+# as far as I can tell, they aren't actually used....
+sed -i'.bak' 's/__int128/__INT64_TYPE__/g' build/*.c
+sed -i'.bak' 's/__uint128/__UINT64_TYPE__/g' build/*.c
 m68k-elf-gcc -nostdlib -c -Iinclude/ -o build/enter.o build/enter.c
+green_echo "(The integer overflow warnings are due to a hack necessary to compile Zig to C on platforms without 128-bit integers in C.)"
 
 green_echo "Compiling 68k assembly..."
-m68k-elf-gcc src/init.s build/*.o  -o "build/linked" -nostdlib
+m68k-elf-gcc src/*.s build/*.o  -o "build/linked" -nostdlib
 
 green_echo "Converting to floppy disk image..."
 m68k-elf-objcopy -O binary build/linked floppy.img
